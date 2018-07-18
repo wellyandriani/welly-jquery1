4f85872a40416e3fa6cba2f7c66b4ba0ddc374d7
wellyandriani <wellyandriani97@gmail.com>
2018-07-18 15:16:17 +0700

    add index js for query

---
+++ index.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $("h1").css("color", " rgb(177, 51, 72)");
+});
+
+$(document).ready(function () {
+    $("h2").css("color", "blue");
+});
+
+
+$(document).ready(function () {
+    $(".h3").css("border", "3px solid red");
+});
+
+$(document).ready(function () {
+    $('input').css("border", "3px solid rgb(250, 138, 156)");
+    $('input').css("border-radius", "0px");
+    $('input').css("padding", "10px");
+    $('input').css("margin-top", "5px");
+    $('input').css("color", "rgb(177, 51, 72)");
+});
+
+
+$(document).ready(function () {
+
+    
+    $('button#add').on('click', function () {
+  
+        var $newTask = $('#new-task').val();
+
+        if ($newTask === '') {
+
+            $('.warning');
+  
+            $('.success').hide();
+        } else {
+
+            $('.success');
+      
+            $('.warning').hide();
+            
+            var newListItem = '<li>';
+            newListItem += '<label>' + $newTask + '</label>';
+            newListItem += '<button class="delete">x</button>';
+            newListItem += '</li>';
+             
+            $('ul#incomplete-tasks').append(newListItem);
+            $('#new-task').val('');
+        }; 
+    });   
+ 
+    $('ul').on('click', '.delete', function () {
+        $(this).parent().remove();
+        countTask();
+    });  
+});  
